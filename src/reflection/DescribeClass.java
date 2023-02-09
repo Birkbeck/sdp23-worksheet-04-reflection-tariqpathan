90c64f5434843b123eccf7e17b06a8c80294ea28
Tariq Pathan <tariq047@gmail.com>
2023-02-09 21:01:41 +0000

    added DescribeClass stuff

--- src/reflection/DescribeClass.java
+++ src/reflection/DescribeClass.java
@@ -1,10 +1,31 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+import java.util.Arrays;
+
 public class DescribeClass {
     public static void main(String[] args) {
-        if (args.length != 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+        if (args.length != 1) {
+            System.out.println("Usage: DescribeClass.java <ClassName>");
+        } else {
+            try {
+                var classInfo = java.lang.Class.forName(args[0]);
+                System.out.println("entered: " + args[0]);
+                getClassInformation(classInfo);
+            } catch (ClassNotFoundException e) {
+                System.out.println(args[0] + " wasn't found");
+            }
+        }
+    }
 
-        // TODO: implement the functionality for Question 1
+    private static void getClassInformation(Class<?> classInfo) {
+        System.out.println(classInfo.getConstructors().length);
+        Constructor<?>[] constructors = classInfo.getConstructors();
+        Arrays.stream(constructors).limit(4).forEach(System.out::println);
+        System.out.println(constructors.length > 4 ? "..." : "");
+//        for (Constructor c : constructors) {
+//            System.out.println(c.getName());
+//        }
+        //System.out.println(classInfo.getDeclaredConstructor().length);
     }
 }
