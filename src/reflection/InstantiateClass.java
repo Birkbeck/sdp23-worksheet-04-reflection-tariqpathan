5494f95cb4e2e4c62e197b166c5c423831f37dcc
Tariq Pathan <tariq047@gmail.com>
2023-02-13 21:01:45 +0000

    implementing InstantiateClass

--- src/reflection/InstantiateClass.java
+++ src/reflection/InstantiateClass.java
@@ -1,10 +1,35 @@
 package reflection;
 
+import java.lang.reflect.Constructor;
+
 public class InstantiateClass {
     public static void main(String[] args) {
-        if (args.length < 1)
-            System.out.println("Usage: "); // TODO: describe how to use the utility
+        if (args.length < 0)
+            System.out.println("Usage: java.lang.Class <params separated with space>");
+        else
+        {
+            try {
+                var cls = java.lang.Class.forName("java.lang.String");
+                Constructor constructor = getConstructor(cls, args.length);
+                Class[] parameterTypes = constructor.getParameterTypes();
+                java.lang.reflect.Constructor.newInstance();
 
-        // TODO: implement the functionality for Question 2
+            } catch (ClassNotFoundException e) {
+                System.out.println(args[0] + " is not a valid class name");
+            } catch (NoSuchMethodException e) {
+                System.out.println("A valid constructor was not found");
+            }
+        }
     }
+
+    private static Constructor getConstructor(Class<?> cls, int argsLength) throws NoSuchMethodException {
+        Constructor[] constructorsArray = cls.getConstructors(); // why not DeclaredConstructors
+        for (Constructor c: constructorsArray) {
+            if (c.getParameterCount() == argsLength - 1) {
+                return c;
+            }
+        }
+        throw new NoSuchMethodException();
+    }
+
 }
