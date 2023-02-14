0b5b8c9467639af54a6b7401f819dd1c113fabf3
Tariq Pathan <tariq047@gmail.com>
2023-02-14 18:43:45 +0000

    working on Q3

--- src/reflection/InstantiateClass.java
+++ src/reflection/InstantiateClass.java
@@ -4,33 +4,21 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.Arrays;
 
-// need to handle cases where there are same parameter counts but only one set accepts
-// only strings
 public class InstantiateClass {
-    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalArgumentException, IllegalAccessException{
+    public static void main(String[] args) throws InstantiationException, IllegalArgumentException, IllegalAccessException{
         if (args.length < 0)
             System.out.println("Usage: java.lang.Class <params separated with space>");
         else
         {
             try {
                 var cls = java.lang.Class.forName(args[0]);
-                System.out.println(cls.getName());
                 Constructor constructor = getConstructor(cls, args.length);
-                Class[] parameterTypes = constructor.getParameterTypes();
-                System.out.println(Arrays.asList(parameterTypes));
-
-                // these will be the args used to create the args[0] object
-                // these first need to be converted from strings as they are
-                // taken from the command line and then converted to their
-                // appropriate type
                 Object[] typedArgs = getTypedArgs(constructor, args);
+                if (typedArgs.length != constructor.getParameterCount()) {
+                    throw new IllegalArgumentException();
+                }
                 Object obj = constructor.newInstance(typedArgs);
                 System.out.println(obj.toString());
-//                java.lang.reflect.Constructor.newInstance();
-                // for each parameterType, I need to check that constructor for
-                // that type accepts Strings as an argument, then I need to
-                // create that object, then I need to pass these objects into the
-                // constructor for the class which I wanted to create at args[0]
 
             } catch (ClassNotFoundException e) {
                 System.out.println(args[0] + " is not a valid class name");
@@ -45,12 +33,10 @@ public class InstantiateClass {
     private static Constructor getConstructor(Class<?> cls, int argsLength) throws NoSuchMethodException {
         Constructor[] constructorsArray = cls.getConstructors(); // why not DeclaredConstructors
         for (Constructor c: constructorsArray) {
-
-            if (c.getParameterCount() == argsLength - 1) {
-                System.out.println("lengths match");
-                Arrays.stream(c.getParameterTypes()).forEach(System.out::println);
-                if (Arrays.stream(c.getParameterTypes()).allMatch(String.class::equals)) return c;
+            if (c.getParameterCount() != argsLength - 1) {
+                continue;
             }
+            if (Arrays.stream(c.getParameterTypes()).allMatch(String.class::equals)) return c;
         }
         throw new NoSuchMethodException();
     }
--- test/reflection/Autograder.java
+++ test/reflection/Autograder.java
@@ -2,19 +2,82 @@ package reflection;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.util.ArrayList;
+
 public class Autograder {
 
     static final String CLASS_NAME = ""; // TODO: insert the class name to be tested
 
+    Class cls = Class.forName(CLASS_NAME);
+
+    public Autograder() throws ClassNotFoundException {
+    }
+
     @Test
     public void testMoreThanFourFields() {
-        // TODO: add testing code
+        if (cls.getDeclaredFields().length > 4) {
+            throw new RuntimeException("Class contains more than 4 fields");
+        }
     }
 
     @Test
     public void testNonPrivateFields() {
-        // TODO: add testing code
+        Field[] fields = cls.getDeclaredFields();
+        for (Field f : fields) {
+            if (!Modifier.isPrivate(f.getModifiers())) {
+                throw new RuntimeException(f.getName() + "is a non-private field");
+            }
+        }
     }
 
-    // TODO: add a test for each of the remaining items of Question 3
+    @Test
+    public void testArrayListField() {
+        Field[] fields = cls.getDeclaredFields();
+        for (Field f : fields) {
+            if (f.getType().isAssignableFrom(ArrayList.class)) {
+                throw new RuntimeException(f.getName() + " contains a field of ArrayList");
+            }
+        }
+    }
+    @Test
+    public void testMoreThanTwoPrivateHelperMethods() {
+        Method[] methods = cls.getMethods();
+        int countPrivateMethods = 0;
+        for (Method m : methods) {
+            if (Modifier.isPrivate(m.getModifiers()))
+            {
+                countPrivateMethods += 1;
+                if (countPrivateMethods > 2) {
+                    throw new RuntimeException("More than 2 private methods found");
+                }
+            }
+        }
+    }
+    @Test
+    public void testMethodHasThrowsClause() {
+        // TODO: add testing code
+    }
+    @Test
+    public void testMethodReturnsInt() {
+        Method[] methods = cls.getMethods();
+        for (Method m : methods) {
+            if (m.getReturnType().equals(Integer.TYPE)) {
+                throw new RuntimeException("Method " + m.getName() + " returns an int");
+            }
+        }
+    }
+    @Test
+    public void testMissingZeroArgumentConstructor() {
+        Constructor[] constructors = cls.getConstructors();
+        for (Constructor c : constructors) {
+            if (c.getParameterCount() == 0) {
+                return;
+            }
+        }
+        throw new RuntimeException("A zero argument constructor was not found");
+    }
 }
