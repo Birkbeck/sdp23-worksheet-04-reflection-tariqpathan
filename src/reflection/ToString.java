bc87c578ec1f9ec03cbfe0bb178dfed52bc10aaa
Tariq Pathan <tariq047@gmail.com>
2023-02-15 11:26:57 +0000

    Q4, trying some basic reflection stuff

--- src/reflection/ToString.java
+++ src/reflection/ToString.java
@@ -1,8 +1,31 @@
 package reflection;
 
+import java.lang.reflect.Field;
+import java.util.Arrays;
+
 public class ToString {
     public static String toString(Object obj) {
-        // TODO: implement the functionality for Question 4
+
+        Class clazz = obj.getClass();
+        clazz.getName();
+        Field[] fields = clazz.getDeclaredFields();
+        Arrays.stream(fields).forEach(f -> {
+            f.setAccessible(true);
+            try {
+                Object value = f.get(obj);
+                System.out.print("The name of the field: " + f.getName());
+                System.out.print(" and it's value is " + value + "\n");
+            } catch (IllegalAccessException e) {
+                //throw new RuntimeException(e);
+            }
+        });
+
         return null;
+    }
+
+    public static void main(String[] args) {
+        Person p = new Person("T", 23, 78f);
+        ToString t = new ToString();
+        t.toString(p);
     }
 }
